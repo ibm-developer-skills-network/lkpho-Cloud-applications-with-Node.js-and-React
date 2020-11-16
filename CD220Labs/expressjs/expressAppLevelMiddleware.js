8331240208cca9d575b10cc7084c83510b2b74a2
LavanyaTS <lavanys@skillup.tech>
2020-11-16 20:07:35 +0530

    Exercise files for Routing, middleware and Templates
    Added exercise files.

---
+++ CD220Labs/expressjs/expressAppLevelMiddleware.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = new express();
+
+app.use(function (req, res, next) {
+    if (req.query.password !== "pwd123") {
+        return res.status(402).send("This user cannot login ");    
+        res.
+    }
+    console.log('Time:', Date.now())
+    next()
+  })
+
+app.get("/", (req,res) => {
+    return res.send("Hello World!");
+});
+
+app.listen(3333, () => {
+    console.log(`listening at http://localhost:3333`)
+})
+
