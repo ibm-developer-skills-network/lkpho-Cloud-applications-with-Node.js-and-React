41088cc64481a85561ee4e248e13bee22cd31807
lavskillup <73978832+lavskillup@users.noreply.github.com>
2020-11-18 15:59:29 +0530

    Add files via upload

---
+++ CD220Labs/async_callback/asyncAxiosRequest.js
@@ -0,0 +1,18 @@
+const axios = require('axios').default;
+
+
+const connectToURL = (url)=>{
+    const req = axios.get(url);
+    console.log(req);
+    req.then(resp => {
+        console.log("Fulfilled")
+        console.log(resp.data);
+    })
+    .catch(err => {
+        console.log("Rejected")
+    });
+}
+//Valid URL
+connectToURL('https://raw.githubusercontent.com/ibm-developer-skills-network/lkpho-Cloud-applications-with-Node.js-and-React/master/CD220Labs/async_callback/sampleDate.json');
+//Invali URL
+connectToURL('https://raw.githubusercontent.com/ibm-developer-skills-network/lkpho-Cloud-applications-with-Node.js-and-React/master/CD220Labs/async_callback/sampleDate');
