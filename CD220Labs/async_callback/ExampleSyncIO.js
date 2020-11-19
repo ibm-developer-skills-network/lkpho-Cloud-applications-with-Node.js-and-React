071dd7f748576c2ca505763946b9e19d7608b313
lavskillup <73978832+lavskillup@users.noreply.github.com>
2020-11-19 12:08:54 +0530

    Update ExampleSyncIO.js

--- CD220Labs/async_callback/ExampleSyncIO.js
+++ CD220Labs/async_callback/ExampleSyncIO.js
@@ -1,24 +1,21 @@
 // Requiring fs module - fs is used for File I/O
 var fs = require('fs');
 
-var prompt = require('prompt-sync')();
-var filename = prompt('What is the name of the file ?');
-
 var filename1 = "courseDetails.json"
 var filename2 = "sampleData.json"
 
 function readFile1(filename1) {
- // Reading the file Synchronously - Blocking rest of execution
- var data = fs.readFileSync(filename1);
- console.log("\n\nThe content of the file is \n\n"+data);
- console.log("Completed reading file1");
+   // Reading the file Synchronously - Blocking rest of execution
+   var data = fs.readFileSync(filename1);
+   console.log("\n\nThe content of the file is \n\n"+data);
+   console.log("Completed reading file1");
 }
 
 function readFile2(filename2) {
- // Reading the file Synchronously - Blocking rest of execution
- var data = fs.readFileSync(filename2);
- console.log("\n\nThe content of the file is \n\n"+data);
- console.log("Completed reading file2");
+   // Reading the file Synchronously - Blocking rest of execution
+   var data = fs.readFileSync(filename2);
+   console.log("\n\nThe content of the file is \n\n"+data);
+   console.log("Completed reading file2");
 }
 
 console.log('Before the reading the file-1');
