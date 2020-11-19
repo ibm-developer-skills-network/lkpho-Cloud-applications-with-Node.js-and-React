31214ebfce95f26dd07a7f0d235f134ef05bfe63
lavskillup <73978832+lavskillup@users.noreply.github.com>
2020-11-19 12:44:24 +0530

    Update asyncAxiosRequest.js

--- CD220Labs/async_callback/asyncAxiosRequest.js
+++ CD220Labs/async_callback/asyncAxiosRequest.js
@@ -9,10 +9,11 @@ const connectToURL = (url)=>{
         console.log(resp.data);
     })
     .catch(err => {
-        console.log("Rejected")
+        console.log("Rejected for url "+url)
+        console.log(err.toString())
     });
 }
 //Valid URL
-connectToURL('https://raw.githubusercontent.com/ibm-developer-skills-network/lkpho-Cloud-applications-with-Node.js-and-React/master/CD220Labs/async_callback/sampleDate.json');
+connectToURL('https://raw.githubusercontent.com/ibm-developer-skills-network/lkpho-Cloud-applications-with-Node.js-and-React/master/CD220Labs/async_callback/sampleData.json');
 //Invali URL
-connectToURL('https://raw.githubusercontent.com/ibm-developer-skills-network/lkpho-Cloud-applications-with-Node.js-and-React/master/CD220Labs/async_callback/sampleDate');
+connectToURL('https://raw.githubusercontent.com/ibm-developer-skills-network/lkpho-Cloud-applications-with-Node.js-and-React/master/CD220Labs/async_callback/sampleDate.json');
