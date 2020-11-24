ed9f43338993faf0690a060a0c15531d792421a0
LavanyaTS <lavanys@skillup.tech>
2020-11-24 12:16:21 +0530

    Made changes to include language options

--- CD220Labs/expressjs/expressTranslator.js
+++ CD220Labs/expressjs/expressTranslator.js
@@ -5,20 +5,24 @@ dotenv.config();
 const app = new express();
 app.use(express.static('cad220_staticfiles'))
 
+function getLanguageTranslator() {
+  let api_key = process.env.API_KEY;
+  let api_url = process.env.API_URL;
+  
+  const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
+  const { IamAuthenticator } = require('ibm-watson/auth');
+  
+  const languageTranslator = new LanguageTranslatorV3({
+    version: '2018-05-01',
+    authenticator: new IamAuthenticator({
+      apikey: api_key,
+    }),
+    serviceUrl: api_url,
+  });
+  return languageTranslator;
+}
 function translate(textToTranslate,res) {
-    let api_key = process.env.API_KEY;
-    let api_url = process.env.API_URL;
-    
-    const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
-    const { IamAuthenticator } = require('ibm-watson/auth');
-    
-    const languageTranslator = new LanguageTranslatorV3({
-      version: '2018-05-01',
-      authenticator: new IamAuthenticator({
-        apikey: api_key,
-      }),
-      serviceUrl: api_url,
-    });
+    let languageTranslator = getLanguageTranslator();
 
     const translateParams = {
         text: textToTranslate,
@@ -33,9 +37,29 @@ function translate(textToTranslate,res) {
         });
 }
 
+function getLanguages(res) {
+    let languageTranslator = getLanguageTranslator();
+    languageTranslator.listModels()
+    .then(translationModels => {
+      let models = translationModels.result.models;
+      let modelNames = models.map((model)=>{
+        return model.name
+      });
+      res.send(modelNames);
+    })
+    .catch(err => {
+      res.send(err.toString());
+    });
+  
+}
+
 app.get("/translate", (req,res) => {
     let textToTranslate = req.query.textToTranslate;
     translate(textToTranslate,res);
+});
+
+app.get("/translators", (req,res) => {
+  getLanguages(res);
 });
 
 app.listen(3333, () => {
