842cd353a23aec87a12ad96f81c7e8fce08584be
lavskillup <73978832+lavskillup@users.noreply.github.com>
2023-03-07 15:48:28 +0530

    Update asyncPromise.js

--- CD220Labs/async_callback/asyncPromise.js
+++ CD220Labs/async_callback/asyncPromise.js
@@ -1,8 +1,8 @@
-var prompt = require('prompt-sync')();
-var fs = require('fs');
+let prompt = require('prompt-sync')();
+let fs = require('fs');
 
 const methCall = new Promise((resolve,reject)=>{
-    var filename = prompt('What is the name of the file ?');
+    let filename = prompt('What is the name of the file ?');
     try {
       const data = fs.readFileSync(filename, {encoding:'utf8', flag:'r'}); 
       resolve(data);
