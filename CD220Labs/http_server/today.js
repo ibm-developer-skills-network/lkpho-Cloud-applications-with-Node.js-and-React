b56b0e53728d5f14433a296d96b0173c6e7936c1
lavskillup <73978832+lavskillup@users.noreply.github.com>
2023-03-07 09:02:41 +0530

    Update today.js

--- CD220Labs/http_server/today.js
+++ CD220Labs/http_server/today.js
@@ -1,4 +1,4 @@
 module.exports.getDate = function getDate() {
-    var aestTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane"});
+    let aestTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane"});
     return new Date(aestTime);
 }
