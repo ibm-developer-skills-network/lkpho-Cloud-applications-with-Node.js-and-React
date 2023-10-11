e7e644262a0b1a9a958695aa912956353145a246
vedanti <vedanti.r.bagade@gmail.com>
2023-10-11 14:54:44 +0530

    index-with-require.js added

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -3,10 +3,21 @@ const today = require('./today');
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end(`Hello, World! The date today is ${today.getDate()}`);
+  let dateVal = today.getDate();
+  let greeting = "It is still not morning."
+  if (dateVal.getHours()>6 && dateVal.getHours()<12) {
+    greeting = "Good Morning!"
+  } else if (dateVal.getHours()>=12 && dateVal.getHours()<18) {
+    greeting = "Good Afternoon!"
+  } else if (dateVal.getHours()>=18 && dateVal.getHours()<21) {
+    greeting = "Good Evening!"
+  } else if (dateVal.getHours()>=21 && dateVal.getHours()<24) {
+    greeting = "Good Night!"
+  }
+  res.end(`Hello, ${greeting}`);
 }
 
-const port = 8080;
+const port = 8080; //port number
 const server = http.createServer(requestListener);
 console.log('server listening on port: ' + port);
 server.listen(port);
