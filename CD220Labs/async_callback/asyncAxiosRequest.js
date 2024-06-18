0d6ed1fc51fd4a3836fd0ab5a31ff29a570ca0b2
RajshriSkillup <132663983+RajshriSkillup@users.noreply.github.com>
2024-06-18 22:08:08 +0530

    Update asyncAxiosRequest.js

--- CD220Labs/async_callback/asyncAxiosRequest.js
+++ CD220Labs/async_callback/asyncAxiosRequest.js
@@ -1,19 +1,29 @@
+// Requiring axios module for making HTTP requests
 const axios = require('axios').default;
 
-
-const connectToURL = (url)=>{
+// Function to connect to a URL and handle the response
+const connectToURL = (url) => {
+    // Sending a GET request to the specified URL using axios
     const req = axios.get(url);
+    // Logging the initial promise object
     console.log(req);
+    // Handling the promise resolution
     req.then(resp => {
-        console.log("Fulfilled")
+        // Logging the fulfillment message
+        console.log("Fulfilled");
+        // Logging the response data
         console.log(resp.data);
     })
+    // Handling the promise rejection
     .catch(err => {
-        console.log("Rejected for url "+url)
-        console.log(err.toString())
+        // Logging the rejection message with the URL
+        console.log("Rejected for url " + url);
+        // Logging the error message
+        console.log(err.toString());
     });
 }
-//Valid URL
+
+// Valid URL
 connectToURL('https://raw.githubusercontent.com/ibm-developer-skills-network/lkpho-Cloud-applications-with-Node.js-and-React/master/CD220Labs/async_callback/sampleData.json');
-//Invali URL
+// Invalid URL
 connectToURL('https://raw.githubusercontent.com/ibm-developer-skills-network/lkpho-Cloud-applications-with-Node.js-and-React/master/CD220Labs/async_callback/sampleDate.json');
