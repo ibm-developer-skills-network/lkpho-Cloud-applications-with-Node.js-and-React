fb9f2f473dc69835ef976bd2fa47f2b22f1ed86f
RajshriSkillup <132663983+RajshriSkillup@users.noreply.github.com>
2024-06-18 22:06:55 +0530

    Update ExampleAsyncCallBack.js

--- CD220Labs/async_callback/ExampleAsyncCallBack.js
+++ CD220Labs/async_callback/ExampleAsyncCallBack.js
@@ -1,8 +1,11 @@
-//This method will be provided as a parameter
+// This method will be provided as a parameter
 function firstCallBackMethod() {
-    console.log("Inside the first call back method")
+    // Logging a message inside the callback method
+    console.log("Inside the first call back method");
 }
 
-console.log("Going to call setTimeOut with a delay of 5 seconds")
-//Call the function firstCallBackMethod after a delay using setTimeOut
-setTimeout(firstCallBackMethod,5000);
+// Log message before calling setTimeout
+console.log("Going to call setTimeout with a delay of 5 seconds");
+
+// Call the function firstCallBackMethod after a delay using setTimeout
+setTimeout(firstCallBackMethod, 5000);
