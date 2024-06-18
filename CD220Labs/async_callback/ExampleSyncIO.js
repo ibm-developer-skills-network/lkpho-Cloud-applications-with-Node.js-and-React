42397023236cb5fdae2159e7fd9b80483a50c996
RajshriSkillup <132663983+RajshriSkillup@users.noreply.github.com>
2024-06-18 22:05:29 +0530

    Update ExampleSyncIO.js

--- CD220Labs/async_callback/ExampleSyncIO.js
+++ CD220Labs/async_callback/ExampleSyncIO.js
@@ -1,27 +1,33 @@
 // Requiring fs module - fs is used for File I/O
 let fs = require('fs');
-
-let filename1 = "courseDetails.json"
-let filename2 = "sampleData.json"
-
+ 
+// Define the filenames to be read
+let filename1 = "courseDetails.json";
+let filename2 = "sampleData.json";
+ 
+// Function to read the first file synchronously
 function readFile1(filename1) {
-   // Reading the file Synchronously - Blocking rest of execution
-   let data = fs.readFileSync(filename1);
-   console.log("\n\nThe content of the file is \n\n"+data);
-   console.log("Completed reading file1");
+    // Reading the file Synchronously - Blocking rest of execution
+    let data = fs.readFileSync(filename1);
+    console.log("\n\nThe content of the file is \n\n" + data);
+    console.log("Completed reading file1");
 }
-
+ 
+// Function to read the second file synchronously
 function readFile2(filename2) {
-   // Reading the file Synchronously - Blocking rest of execution
-   let data = fs.readFileSync(filename2);
-   console.log("\n\nThe content of the file is \n\n"+data);
-   console.log("Completed reading file2");
+    // Reading the file Synchronously - Blocking rest of execution
+    let data = fs.readFileSync(filename2);
+    console.log("\n\nThe content of the file is \n\n" + data);
+    console.log("Completed reading file2");
 }
-
-console.log('Before the reading the file-1');
+ 
+// Log message before reading the first file
+console.log('Before reading file-1');
 readFile1(filename1);
-
-console.log('Before the reading the file-2');
+ 
+// Log message before reading the second file
+console.log('Before reading file-2');
 readFile2(filename2);
-
+ 
+// Log message after reading both files
 console.log('All done!');
