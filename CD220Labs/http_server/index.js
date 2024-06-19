d1e329876935d974cfadd4689372ab883d155370
RajshriSkillup <132663983+RajshriSkillup@users.noreply.github.com>
2024-06-19 13:41:50 +0530

    Update index.js

--- CD220Labs/http_server/index.js
+++ CD220Labs/http_server/index.js
@@ -1,11 +1,18 @@
+ // Import the HTTP module
 const http = require('http');
 
+// Define the request listener function
 const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('Hello, World!');
-}
+    res.writeHead(200); // Set the status code to 200 (OK)
+    res.end('Hello, World!'); // Send the response "Hello, World!"
+};
 
+// Define the port number
 const port = 8080;
+
+// Create an HTTP server using the request listener function
 const server = http.createServer(requestListener);
-console.log('server listening on port: ' + port);
-server.listen(8080);
+
+// Start the server and listen on the specified port
+server.listen(port);
+console.log('Server listening on port: ' + port);
