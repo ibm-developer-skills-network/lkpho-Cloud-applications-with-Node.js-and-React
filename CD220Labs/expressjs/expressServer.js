79b3ce6bbd9ac576599c92ef70e090e7175348af
RajshriSkillup <132663983+RajshriSkillup@users.noreply.github.com>
2024-06-19 13:37:17 +0530

    Update expressServer.js

--- CD220Labs/expressjs/expressServer.js
+++ CD220Labs/expressjs/expressServer.js
@@ -1,26 +1,34 @@
+ // Import the Express.js library
 const express = require('express');
+
+// Create an instance of an Express application
 const app = new express();
 
+// Initialize an array to store login details
 let loginDetails = [];
 
-app.get("/",(req,res)=>{
-    res.send("Welcome to the express server")
-})
+// Define the root route to send a welcome message
+app.get("/", (req, res) => {
+    res.send("Welcome to the express server");
+});
 
-app.get("/loginDetails",(req,res)=>{
+// Define a route to send login details as a JSON string
+app.get("/loginDetails", (req, res) => {
     res.send(JSON.stringify(loginDetails));
-})
+});
 
-app.post("/login/:name",(req,res)=>{
-    loginDetails.push({"name":req.params.name,"login_time":new Date()});
-    res.send(req.params.name + ", You are logged in!")
-})
+// Define a route to handle login requests and store login details
+app.post("/login/:name", (req, res) => {
+    loginDetails.push({ "name": req.params.name, "login_time": new Date() });
+    res.send(req.params.name + ", You are logged in!");
+});
 
-app.get("/:name",(req,res)=>{
-    res.send("Hello "+req.params.name)
-})
+// Define a dynamic route to greet users by name
+app.get("/:name", (req, res) => {
+    res.send("Hello " + req.params.name);
+});
 
+// Start the server and listen on port 3333
 app.listen(3333, () => {
-    console.log(`Listening at http://localhost:3333`)
-})
-
+    console.log(`Listening at http://localhost:3333`);
+});
