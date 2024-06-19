a6937c2624eed33a52e0a4b473bab31a88270357
RajshriSkillup <132663983+RajshriSkillup@users.noreply.github.com>
2024-06-19 13:43:17 +0530

    Update index-with-require.js

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -1,12 +1,22 @@
+ // Import the HTTP module
 const http = require('http');
+
+// Import the 'today' module
 const today = require('./today');
 
+// Define the request listener function
 const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end(`Hello, World! The date today is ${today.getDate()}`);
-}
+    res.writeHead(200); // Set the status code to 200 (OK)
+    // Send the response with the current date from the 'today' module
+    res.end(`Hello, World! The date today is ${today.getDate()}`);
+};
 
+// Define the port number
 const port = 8080;
+
+// Create an HTTP server using the request listener function
 const server = http.createServer(requestListener);
-console.log('server listening on port: ' + port);
+
+// Start the server and listen on the specified port
 server.listen(port);
+console.log('Server listening on port: ' + port);
